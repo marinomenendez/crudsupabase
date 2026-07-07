380d08373f492efa4e90c5495fc3ce64028d1f70
marino <marino.menendez@gmail.com>
2026-07-07 13:53:17 +0200

    update/delete en curso

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -9,12 +9,18 @@ export const getProducts = async (req,res) => {
     res.render("productos", {productos: data}); //lo renderizamos a una plantilla productos, ya que en json en el navegador queda feo
 }
 
+
+export const nuevoFormulario = async(req, res) => {
+    res.render('nuevo', { title: 'Pagina de productos', message: 'Nuevo producto'});
+}
+
 export const addProduct = async (req,res) => {
     console.log("Añadir producto");
     const {nombre, unidades, precio} = req.body;
     if (!nombre) {
         res.send("El nombre es obligatorio");
     }
+
     const insertar = await supabase.from('productos').insert(
         {
         "nombre": req.body.nombre,
@@ -26,6 +32,42 @@ export const addProduct = async (req,res) => {
     res.redirect("/");
 }
 
-export const nuevoFormulario = async(req, res) => {
-    res.render('nuevo', { title: 'Pagina de productos', message: 'Nuevo producto'});
+export const actualizarFormulario = async(req, res) => {
+    res.render('actualizar');
+}
+
+export const updateProduct = async (req,res) => {
+    console.log("Actualizar producto");
+    const {id, nombre, unidades, precio} = req.body;
+    if (!nombre) {
+        res.send("El nombre es obligatorio");
+    }
+
+    const actualizar = await supabase.from('productos').update(
+        {
+        "id": Number(req.body.id),
+        "nombre": req.body.nombre,
+        "unidades": Number(req.body.unidades),
+        "precio": Number(req.body.precio)
+        }
+    );
+    //res.json(actualizar);
+    res.redirect("/");
+}
+
+export const eliminarFormulario = async(req, res) => {
+    res.render('eliminar');
+}
+
+export const deleteProduct = async (req,res) => {
+    console.log("Eliminar producto");
+    const {id} = req.body;
+
+    const borrar = await supabase.from('productos').delete(
+        {
+        "id": Number(req.body.id)
+        }
+    );
+    //res.json(borrar);
+    res.redirect("/");
 }
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { getProducts, addProduct, nuevoFormulario } from "../controllers/product-controller.js";
+import { getProducts, addProduct, nuevoFormulario, actualizarFormulario, eliminarFormulario, updateProduct, deleteProduct } from "../controllers/product-controller.js";
 
 const router = Router();
 router.get("/", getProducts);
 //router.get("/add", addProduct);
+
+//Rutas para añadir producto
 router.get("/nuevo", nuevoFormulario);
 router.post("/addProduct", addProduct); //Segundo parametro es la funcion que enlaza con el controlador
+
+//Rutas para actualizar producto
+router.get("/actualizar", actualizarFormulario);
+router.post("/updateProduct", updateProduct);
+
+//Rutas para eliminar producto
+router.get("/eliminar", eliminarFormulario);
+router.post("/deleteProduct", deleteProduct);
 
 export default router;
